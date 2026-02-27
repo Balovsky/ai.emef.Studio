0d9f457cb19419c25e89e22f80edda24295c0229
Balovsky <mariuszfranik98@gmail.com>
2026-02-27 22:18:30 +0100

    create gallery full

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 // import { defineConfig } from 'astro/config';
 
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://km-studio.pl',
+  integrations: [tailwind(), sitemap()]
 });
 // https://astro.build/config
 // export default defineConfig({});
